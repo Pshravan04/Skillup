c3675f0ca833df1d9bdf5853274e1d91d627871c
shravan phutane <shravanphutanr@gmail.com>
2026-02-11 18:17:37 +0530

    feat: Implement Axios API client with a request interceptor to include JWT tokens for authentication.

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://localhost:5000/api', // Backend URL
+    baseURL: `${process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000'}/api`,
 });
 
 // Add a request interceptor to include the JWT token
